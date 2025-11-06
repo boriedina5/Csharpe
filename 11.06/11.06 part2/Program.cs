af60f2dca76b848273d11c2ec82a4003f8b16c43
Bori Edina <bori.edina.gabriella@blathy.info>
2025-11-06 13:11:44 +0100

    Kész

---
+++ 11.06/11.06 part2/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace _11._06_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> numbers = new List<int>();
+           string[] rows = File.ReadAllLines("input.txt");
+           foreach (string row in rows) {
+                numbers.Add(int.Parse(row));
+           }
+           //Van-e a sorozatban pozitív szám?
+           bool positive = false;
+           int index = 0;
+            while (index < numbers.Count && !(numbers[index] > 0))
+            {
+                index++;
+            }
+            positive = index < numbers.Count;
+            Console.WriteLine(positive);
+            
+            //Hány eleme van a sorozatnak ?
+            int listElementCount = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                listElementCount++;
+            }
+            Console.WriteLine(listElementCount);
+
+            // Mennyi a sorozatban található legkisebb szám?
+            int min = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (min > numbers[i]) { 
+                    min = numbers[i];
+                }
+            }
+            Console.WriteLine(min);
+
+            //Mennyi a sorozatban található számok átlagának a fele?
+            int sum = 0;
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine(sum / numbers.Count / 2);
+
+            //Igaz-e, hogy minden szám pozitív?
+            bool existNegativ = false;
+            index = 0;
+            while (index < numbers.Count && !(numbers[index] < 0))
+            {
+                index++;
+            }
+            existNegativ = index < numbers.Count;
+            Console.WriteLine(existNegativ);
+
+            //Hány páratlan szám található a sorozatban?
+            int listUeElementCount = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 1) {
+                    listUeElementCount++;
+                }
+            }
+            Console.WriteLine(listUeElementCount);
+
+            //Van-e a sorozatban olyan negatív szám, amelyet újabb negatív követ?
+            bool existThisSen = false;
+            index = 1;
+
+            while (index < numbers[index] && !(numbers[index-1] < 0 && numbers[index] < 0)) { 
+                index++;
+            }
+            existThisSen = index < numbers.Count;
+            Console.WriteLine(existThisSen);
+
+            //Írjuk ki az utolsó 19-tel osztható szám indexét!
+            int dIndex = 0;
+            for (int i = 0; i < numbers.Count; index++)
+            {
+                if (numbers[i] % 19 == 0)
+                {
+                    dIndex = i;
+                }
+            }
+            Console.WriteLine(dIndex);
+            //Írjuk ki a sorozatban található 5-tel osztható számokat!
+            List<int> dividedFive = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 5 == 0)
+                {
+                    dividedFive.Add(numbers[i]);
+                }
+            }
+            for (int i = 0; i < dividedFive.Count; i++)
+            {
+                Console.WriteLine(dividedFive[i]);
+            }
+        }
+    }
+}
---
+++ 11.06/11.06/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.ComponentModel;
+
+namespace _11._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            //beolvasás
+                string[] rows = File.ReadAllLines("input.txt");
+                foreach(string row in rows)
+                {
+                    numbers.Add(int.Parse(row));
+                }
+
+            //Hány eleme van a sorozatnak?
+                int listElementCount = 0;
+                for (int i = 0; i < numbers.Count; i++) { 
+                    listElementCount++;
+                }
+                Console.WriteLine(listElementCount);
+
+            //Van-e a sorozatban negatív szám
+            bool exist = false;
+                int index = 0;
+                while (index < numbers.Count && !(numbers[index] < 0))
+                {
+                    index++;
+                }
+                exist = index < numbers.Count;
+                Console.WriteLine(exist);
+
+                //LINQ műveletek - lambda kifejezést vár műveletnek
+                exist = numbers.Any(x => x < 0);// x = az a szám, amit vizsgálunk a tömbböl
+                Console.WriteLine(exist);
+
+            //Hány páros szám van a sorozatban
+                int count = 0;
+                for (int i = 0; i < numbers.Count; i++) {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                //LINQ
+                count = numbers.Count(x => x % 2 == 0);
+            //Mennyi a sorozatban található legnagyobb szám?
+                int maxValue = numbers[0];
+                for (int i = 1; i < numbers.Count; i++) { 
+                    if (numbers[i] > maxValue)
+                    {
+                        maxValue = numbers[i];
+                    }
+                }
+                Console.WriteLine(maxValue);
+                //LINQ
+                maxValue = numbers.Max();
+                Console.WriteLine(maxValue);
+
+            //Írjuk ki a sorozatban található 10-zel osztható számokat!
+                List <int> dividedTen = new List<int>();
+                for (int i = 0; i < numbers.Count; i++) {
+                if (numbers[i] % 10 == 0) { 
+                    dividedTen.Add(numbers[i]);
+                }
+                }
+                for (int i = 0; i < dividedTen.Count; i++)
+                {
+                    Console.WriteLine(dividedTen[i]);
+                }
+                
+                //Írjuk ki az első 29-cel osztható szám indexét! - Kiválasztás
+                index = 0;
+                while (!(numbers[index] % 29 == 0)) {
+                    index++;
+                }
+                Console.WriteLine(index);
+                //LINQ
+                index = numbers.FindIndex(x => x % 29 == 0);
+            //Igaz-e, hogy minden szám páros?
+                bool isThereUnequalledNumber = false;
+                index = 0;
+                while (index < numbers.Count && !(numbers[index] % 2 == 1)) { 
+                    index ++;
+                }
+                isThereUnequalledNumber = index < numbers.Count;
+
+            // Mennyi a sorozatban található számok átlaga?
+            int s = 0;
+                for (int i = 0; i < numbers.Count; index++) {
+                    s += i;
+                }
+                Console.WriteLine((double)s / numbers.Count);
+                //LINQ
+                s = numbers.Sum();
+                Console.WriteLine((double)s / numbers.Count);
+
+            //Írjuk ki az utolsó 17-tel osztható szám indexét!
+            
+            int dIndex = 0;
+            for (int i = 0; i < numbers.Count; index++)
+            {
+                if (numbers[i] % 17 == 0) {
+                    
+                    dIndex = i;
+                }
+            }
+            Console.WriteLine(dIndex);
+            //LINQ
+            s = numbers.Sum();
+            Console.WriteLine((double)s / numbers.Count);
+            // Írjuk ki az első 33-mal osztható szám indexét! - keresés
+            exist = false;
+                index = 0;
+                while (index < numbers.Count && !(numbers[index] % 33 == 0 )) {
+                    index++;
+                }
+                exist = index < numbers.Count; ;
+                string output = exist ? $"Van {index}. helyen" : "Nincs";
+                Console.WriteLine(output);
+                //LINQ
+                exist = numbers.Any(x => x % 33 == 0);
+                if(exist){
+                    index = numbers.FindIndex(x => x % 33 == 0);
+                    Console.WriteLine(index);
+                }
+            
+
+        }
+    }
+}
